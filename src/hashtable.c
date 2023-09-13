ea10c709cdc0314521533504dd8961f31e9a34e5
clayton williams <clayton.e.williams15.mil@mail.mil>
2023-09-13 08:54:11 -0400

    Tests written and passing for hash_table_create.

--- src/hashtable.c
+++ src/hashtable.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <errno.h>
 #include "hashtable.h"
+
+#define LOAD(x) ((x * 3) / 4)
+
+enum { PTR_SIZE = 8 };
 
 typedef struct entry_t {
         void *data;
@@ -15,6 +20,36 @@ typedef struct hash_t {
         uint32_t curr_size;
 }hash_t;
 
-hash_t *hash_table_create(uint32_t size, hash_f hash_func) {
-        return -1;
+hash_t *hash_table_create(uint32_t size, hash_f hf) {
+        hash_t *table = NULL;
+        
+        if (!hf) {
+                fprintf(stderr, "hash_table_create: Invalid hash_f - NULL\n");
+                goto EXIT;
+        }
+        
+        if (size < 1) {
+                fprintf(stderr, "hash_table_create: Invalid size - Must be positive integer\n");
+                goto EXIT;
+        }
+
+        table = calloc(1, sizeof(*table));
+        if (!table) {
+                perror("hash_table_create");
+                errno = 0;
+                goto EXIT;
+        }
+
+        table->max_cap = size;
+        table->hash_function = hf;
+        table->entries = calloc(table->max_cap, sizeof(entry_t *));
+        if (!table->entries) {
+                perror("hash_table_create");
+                errno = 0;
+                free(table);
+                table = NULL;
+        }
+
+EXIT:
+        return table;
 }
