d06ec55e0bd42740a6486b935e05d9f43eabcd70
clayton williams <clayton.e.williams15.mil@mail.mil>
2023-09-22 08:17:15 -0400

    Tests passing for test_trie.

--- test/test_all.c
+++ test/test_all.c
@@ -4,6 +4,7 @@ extern Suite *test_io_helper(void);
 extern Suite *test_hashtable(void);
 extern Suite *test_player(void);
 extern Suite *test_llist(void);
+extern Suite *test_trie(void);
 
 int main(void)
 {
@@ -13,11 +14,13 @@ int main(void)
 	Suite *test_hashtable_suite = test_hashtable();
 	// Suite *test_player_suite = test_player();
 	Suite *test_llist_suite = test_llist();
+	Suite *test_trie_suite = test_trie();
 
 	// srunner_add_suite(sr, test_io_helper_suite);
 	srunner_add_suite(sr, test_hashtable_suite);
 	// srunner_add_suite(sr, test_player_suite);
 	srunner_add_suite(sr, test_llist_suite);
+	srunner_add_suite(sr, test_trie_suite);
 
 #ifdef VALGRIND
 	srunner_set_fork_status(sr, CK_NOFORK);
--- test/test_trie.c
+++ test/test_trie.c
@@ -2,10 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "../include/trie.h"
+
+START_TEST(test_trie_create) {
+        ck_assert_ptr_ne(trie_create(), NULL);
+}END_TEST
 
 
+START_TEST(test_trie_insert) {
+        trie_t *test_trie = trie_create();
+        char target[] = "test";
+        ck_assert_int_eq(trie_insert(NULL, NULL), 0);
+        ck_assert_int_eq(trie_insert(&test_trie, NULL), 0);
+        ck_assert_int_eq(trie_insert(NULL, target), 0);
+        trie_destroy(&test_trie);
+}END_TEST
 
-static TFun trie_tests[] {
+static TFun trie_tests[] = {
+        test_trie_create,
+        test_trie_insert,
         NULL
 };
 
