304dc830b23f34c344bec5e3353a33c1377b2590
clayton williams <clayton.e.williams15.mil@mail.mil>
2023-09-25 11:46:20 -0400

    search option fixed to display first nfl team.

--- src/player.c
+++ src/player.c
@@ -199,8 +199,9 @@ int print_search_results(char *search_param, hash_t * player_table)
 
 	while (!llist_is_empty(search_results)) {
 		player_t *player = (player_t *) llist_dequeue(search_results);
-		printf("%s\t%s\t%s\n", player->id, player->name,
-		       player->college);
+		team_t *team = (team_t *)llist_dequeue(player->teams);
+		printf("%10s\t%-20s\t%-20s\t%-20s\n", player->id, player->name,
+		       player->college, team->team_name);
 	}
 	free(search_results);
 	exit_status = 1;
